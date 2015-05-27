9a6193f9872fb2543c5b78171512440f2f135ecb
liubaochun <liubaochun@gmail.com>
2015-05-27 11:08:45 +0800

    first commit

---
+++ vexdashboard-common/src/main/java/com/thistech/vexdashboard/common/com/thistech/vexdashboard/common/model/VexBox.java
@@ -0,0 +1,16 @@
+package com.thistech.vexdashboard.common.com.thistech.vexdashboard.common.model;
+
+import com.thistech.vexdashboard.common.util.VexDashboardUtil;
+import com.thistech.common.mongo.model.BaseMongoPersistent;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+import java.io.Serializable;
+
+@Document(collection = "VexBox")
+public class VexBox {
+
+}
---
+++ vexdashboard-common/src/main/java/com/thistech/vexdashboard/common/util/VexDashboardUtil.java
@@ -0,0 +1,8 @@
+package com.thistech.vexdashboard.common.util;
+
+public class VexDashboardUtil {
+
+    private VexDashboardUtil() {}
+
+    public static final String VEXDASHBOARD_NAMESPACE = "http://www.thistech.com/schemas/vexdashboard/1";
+}
