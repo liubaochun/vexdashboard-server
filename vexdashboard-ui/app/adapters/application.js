bc5d4b64e483d93f14599c624f40f4de533d9bac
liubaochun <liubaochun@gmail.com>
2015-07-02 16:29:49 +0800

    Add websockets demo. and make template display application type.

--- vexdashboard-ui/app/adapters/application.js
+++ vexdashboard-ui/app/adapters/application.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
 	host: 'http://localhost:8080',
-	namespace: 'vexdashboard'
+	namespace: 'vexdashboard/core'
 });
--- vexdashboard-ui/app/helpers/application-type.js
+++ vexdashboard-ui/app/helpers/application-type.js
@@ -1,8 +1,15 @@
 import Ember from 'ember';
 
-export function applicationType(params/*, hash*/) {
-	console.log("TEST XXXXX helper");
-  return params;
-}
+export function applicationType(params) {
+       if (!Ember.isBlank(params[0])) {
+               if (params[0] === "COREVEX") {
+                       return "CORE VEX";
+               } else if (params[1] === "DIRECTOR") {
+                       return "VEX DIRECTOR";
+               } else if (params[2] === "FRONTEND") {
+                       return "VEX FRONTEND";
+               }
+       }
+ }
 
 export default Ember.HTMLBars.makeBoundHelper(applicationType);
